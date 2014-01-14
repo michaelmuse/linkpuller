3428f578a20cb5f14b67928b284f3dfd7b0390d1
Michael Muse <michaelmuse@gmail.com>
2014-01-13 19:18:56 -0500

    trying to fix a heroku pre receive hook error message

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,13 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+#
+  #           #           ####ASK CHUCK
+    #       #   #       #  # include Warden::Test::Helpers 
+      #   #       #   #    # Warden.test_mode!
+        #           #      # login_as(@user)
+
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
