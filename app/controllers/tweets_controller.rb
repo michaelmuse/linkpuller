31340833b7f9a9e78d35727a75eb865192f48ab5
Michael Muse <michaelmuse@gmail.com>
2014-01-17 00:24:11 -0500

    validating user is logged in

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-#NEED to not do this if they are signed in, fix in twitternames controller too  # before_filter :authenticate_user!
+before_filter :authenticate_user!
 
   def index
     @data = get_all_tweet_info_for_table(nil) #nil means we dont have a specific twittername
--- app/controllers/twitter_names_controller.rb
+++ app/controllers/twitter_names_controller.rb
@@ -1,5 +1,5 @@
 class TwitterNamesController < ApplicationController
-  # before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   def new
     
